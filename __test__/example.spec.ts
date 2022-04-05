78c6549ea5f6b3db6c34f7a553f639568f744a93
7eo Aldamas <leo.aldamas@gmail.com>
2022-04-04 23:46:32 -0600

    First commit - Project configuration

---
+++ __test__/example.spec.ts
@@ -0,0 +1,5 @@
+describe('My first test', () => {
+    it('My First Test 7eo', () => {
+        expect(true).toBe(true);
+    });
+});
---
+++ index.ts
@@ -0,0 +1,28 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Create Express APP
+const app = express();
+
+const port: string | number = process.env.PORT || 8000;
+
+// Define the firts route of APP
+app.get('/', (request: Request, response: Response) => {
+    // Hello World :D
+    response.send('Hello World - Leobardo Daniel Gonzalez Aldamas - Node');
+});
+
+// Define the firts route of APP
+app.get('/7eo', (request: Request, response: Response) => {
+    // Hello World :D
+    response.send('Leobardo Daniel Gonzalez Aldamas - Node - First Time');
+});
+
+
+// Execute APP
+app.listen(port, () => {
+    console.log(`EXPRESS SERVER: Running at http://localhost:${port}`);
+});
+
